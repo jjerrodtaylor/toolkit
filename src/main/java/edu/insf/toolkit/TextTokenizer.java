987ea79d8937f55b75ddbab28352df1c312bfab4
jamaal.taylor <j.jerrod.taylor@gmail.com>
2014-01-15 17:18:09 -0500

    I can finally tokenize sentences.
    I should have looked up BreakIterator yesterday.

--- src/main/java/edu/insf/toolkit/TextTokenizer.java
+++ src/main/java/edu/insf/toolkit/TextTokenizer.java
@@ -1,6 +1,11 @@
 package edu.insf.toolkit;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
 
 public class TextTokenizer
@@ -8,11 +13,21 @@ public class TextTokenizer
     //http://docs.oracle.com/javase/6/docs/api/java/util/Locale.html#UK
     private Locale locale = null;
     private BreakIterator breakIterator = null;
+    private FileHelper fHelper = new FileHelper();
 
+    public TextTokenizer()
+    {
+        super();
+    }
 
     public Locale getLocale()
     {
         return locale;
+    }
+
+    public void setLocale(Locale locale)
+    {
+        this.locale = locale;
     }
 
     public void setLocale(String language)
@@ -29,4 +44,41 @@ public class TextTokenizer
     {
         locale = new Locale(language,country,variant);
     }
+
+    public ArrayList<String> tokenize(String text, Locale locale){
+        ArrayList<String> sentences = new ArrayList<String>();
+        BreakIterator sentenceIterator = BreakIterator.getSentenceInstance(Locale.US);
+        sentenceIterator.setText(text);
+        int boundary = sentenceIterator.first();
+        int lastBoundary = 0;
+        while (boundary != BreakIterator.DONE) {
+            boundary = sentenceIterator.next();
+            if(boundary != BreakIterator.DONE){
+                sentences.add(text.substring(lastBoundary, boundary));
+            }
+            lastBoundary = boundary;
+        }
+        return sentences;
+    }
+
+    public ArrayList<String> tokenize(String text, String language, String country){
+        ArrayList<String> sentences = new ArrayList<String>();
+        Locale currentLocale = new Locale(language, country);
+        BreakIterator sentenceIterator = BreakIterator.getSentenceInstance(currentLocale);
+        sentenceIterator.setText(text);
+        int boundary = sentenceIterator.first();
+        int lastBoundary = 0;
+        while (boundary != BreakIterator.DONE) {
+            boundary = sentenceIterator.next();
+            if(boundary != BreakIterator.DONE){
+                sentences.add(text.substring(lastBoundary, boundary));
+            }
+            lastBoundary = boundary;
+        }
+        return sentences;
+    }
+
+
+
+
 }
