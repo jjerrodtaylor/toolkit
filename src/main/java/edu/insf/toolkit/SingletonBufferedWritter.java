9ec3a23d6c13a2147233f69c23f4295a5e10cdf6
jamaal.taylor <j.jerrod.taylor@gmail.com>
2014-01-15 17:16:28 -0500

    The other singleton was separated into its own file.

--- src/main/java/edu/insf/toolkit/SingletonBufferedWritter.java
+++ src/main/java/edu/insf/toolkit/SingletonBufferedWritter.java
@@ -1,5 +1,9 @@
 package edu.insf.toolkit;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: jamaaltaylor
@@ -8,4 +12,28 @@ package edu.insf.toolkit;
  * To change this template use File | Settings | File Templates.
  */
 public class SingletonBufferedWritter {
+
+    private static BufferedWriter bw = null;
+
+    public static BufferedWriter getBufferedWritter(String nameOfFile)
+    {
+        if(bw == null)
+        {
+            try
+            {
+                bw = new BufferedWriter(new FileWriter(nameOfFile));
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return bw;
+    }
+
+    public static BufferedWriter getBufferedWritter()
+    {
+        return bw;
+    }
 }
