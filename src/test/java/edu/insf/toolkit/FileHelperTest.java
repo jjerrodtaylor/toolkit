90cb4be1226dc3498e03d6608c6f6c0d4cfe466a
jamaal.taylor <j.jerrod.taylor@gmail.com>
2014-01-16 14:23:03 -0500

    Testing shit.

---
+++ src/test/java/edu/insf/toolkit/FileHelperTest.java
@@ -0,0 +1,11 @@
+package edu.insf.toolkit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jamaaltaylor
+ * Date: 1/16/14
+ * Time: 2:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FileHelperTest {
+}
