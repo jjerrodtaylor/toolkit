0c3c9979a0df95b6bd82feba44c8c5c7a633a08f
jamaal.taylor <j.jerrod.taylor@gmail.com>
2014-01-16 11:52:29 -0500

    Began implementing the IO types with a factory.

---
+++ src/main/java/edu/insf/toolkit/DesignPatterns/Factory/IOBufferedWritter.java
@@ -0,0 +1,11 @@
+package edu.insf.toolkit.DesignPatterns.Factory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jamaaltaylor
+ * Date: 1/15/14
+ * Time: 10:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class IOBufferedWritter {
+}
---
+++ src/main/java/edu/insf/toolkit/DesignPatterns/Factory/IOType.java
@@ -0,0 +1,5 @@
+package edu.insf.toolkit.DesignPatterns.Factory;
+
+public enum IOType {
+    BUFFERED_WRITTER,BUFFERED_READER
+}
