a362868dc69fb60dc9ab21c9c434ddc921d67893
jamaal.taylor <j.jerrod.taylor@gmail.com>
2014-01-17 08:46:59 -0500

    The tests were failing on the build server because all of the paths that I used in my constants file were hardcoded.
    I wrote a function that creates an absolute path where ever the tests
    are run.

--- src/main/java/edu/insf/toolkit/Constants.java
+++ src/main/java/edu/insf/toolkit/Constants.java
@@ -1,5 +1,7 @@
 package edu.insf.toolkit;
 
+import java.io.File;
+
 /**
  * A file of static constants that will be used through out the nn architectures.
  */
@@ -7,14 +9,21 @@ public class Constants
 {
     public static String READ_FILE_NAME = null;
     public static String WRITE_FILE_NAME = null;
-    public static String METAMORPH_ENG = "/Users/jamaaltaylor/Documents/toolkit/src/test/resources/metamorph_eng.pdf";
-    public static String METAMORPH_ESP = "/Users/jamaaltaylor/Documents/toolkit/src/test/resources/metamorph_esp.pdf";
-    public static String METAMORPH_ENG_TXT = "/Users/jamaaltaylor/Documents/toolkit/src/test/resources/metamorph_eng.txt";
-    public static String METAMORPH_ESP_TXT = "/Users/jamaaltaylor/Documents/toolkit/src/test/resources/metamorph_esp.txt";
-    public static String METAMORPH_ENG_TXT_FMT = "/Users/jamaaltaylor/Documents/toolkit/src/test/resources/metamorph_eng_fmt.txt";
-    public static String METAMORPH_ESP_TXT_FMT = "/Users/jamaaltaylor/Documents/toolkit/src/test/resources/metamorph_esp_fmt.txt";
-    public static String TEST_FILE = "/Users/jamaaltaylor/Documents/toolkit/src/test/resources/testfile.txt";
-    public static String METAMORPH_ENG_ESP_TXT = "/Users/jamaaltaylor/Documents/toolkit/src/test/resources/metamorph-en-es.txt";
+    public static String METAMORPH_ENG = Constants.makeFilePath("/src/test/resources/metamorph_eng.pdf");
+    public static String METAMORPH_ESP = Constants.makeFilePath("/src/test/resources/metamorph_esp.pdf");
+    public static String METAMORPH_ENG_TXT = Constants.makeFilePath("src/test/resources/metamorph_eng.txt");
+    public static String METAMORPH_ESP_TXT = Constants.makeFilePath("/src/test/resources/metamorph_esp.txt");
+    public static String METAMORPH_ENG_TXT_FMT = Constants.makeFilePath("/src/test/resources/metamorph_eng_fmt.txt");
+    public static String METAMORPH_ESP_TXT_FMT = Constants.makeFilePath("/src/test/resources/metamorph_esp_fmt.txt");
+    public static String TEST_FILE = Constants.makeFilePath("/src/test/resources/testfile.txt");
+    public static String METAMORPH_ENG_ESP_TXT = Constants.makeFilePath("/src/test/resources/metamorph-en-es.txt");
+
+    public static String makeFilePath(String addition)
+    {
+        String filePath = new File("").getAbsolutePath();
+        filePath = filePath+addition;
+        return filePath;
+    }
 
     public void setREAD_FILE_NAME(String file_name)
     {
