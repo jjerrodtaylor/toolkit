a6512f69566a4182e4d0c77ae66d253518065f6b
jamaal.taylor <j.jerrod.taylor@gmail.com>
2014-01-19 13:32:15 -0500

    Moved file to a different package.

---
+++ src/main/java/edu/insf/toolkit/DesignPatterns/FluidInterfaceHTML/HTMLPage.java
@@ -0,0 +1,148 @@
+package edu.insf.toolkit.DesignPatterns.FluidInterfaceHTML;
+import java.util.ArrayList;
+
+/*
+* For the uninitiated:
+* http://en.wikipedia.org/wiki/Fluent_interface
+* */
+public class HTMLPage
+{
+    public HTMLPage()
+    {
+        super();
+    }
+
+    private String page = "";
+
+    public HTMLPage openHtml()
+    {
+        page = page+"<html>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage closeHtml()
+    {
+        page = page+"</html>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage openTable()
+    {
+        page = page+"<table>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage closeTable()
+    {
+        page = page+"</table>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage openBody()
+    {
+        page = page+"<body>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage closeBody()
+    {
+        page = page+"</body>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage openHead()
+    {
+        page = page+"<head>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage closeHead()
+    {
+        page = page+"</head>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage openDiv()
+    {
+        page = page+"<div>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage closeDiv()
+    {
+        page = page+"</div>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage openTD()
+    {
+        page = page+"<td>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage closeTD()
+    {
+        page = page+"</td>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage openTR()
+    {
+        page = page+"<tr>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage closeTR()
+    {
+        page = page+"</tr>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage openP()
+    {
+        page = page+"<p>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage closeP()
+    {
+        page = page+"</p>" + System.getProperty("line.separator");
+        return this;
+    }
+
+    public HTMLPage addContent(String content)
+    {
+        page = page+content + System.getProperty("line.separator");
+        return this;
+    }
+
+    public String makePage(ArrayList<ArrayList<String>> content)
+    {
+        this.openHtml()
+            .openBody()
+            .openTable();
+
+            for(int i=0;i<content.get(0).size(); i++)
+            {
+                this.openTR()
+                        .openTD()
+                            .openP()
+                                .addContent(content.get(0).get(i))
+                            .closeP()
+                        .closeTD()
+                        .openTD()
+                            .openP()
+                                .addContent(content.get(1).get(i))
+                            .closeP()
+                        .closeTD()
+                    .closeTR();
+            }
+        this.closeTable()
+            .closeBody()
+            .closeHtml();
+
+        return this.page;
+    }
+
+
+}
