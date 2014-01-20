7e029f895a6205c39a120f4d1143b3e8391f8304
jamaal.taylor <j.jerrod.taylor@gmail.com>
2014-01-20 14:05:21 -0500

    Class to represent the alpha-2 code language strings.
    http://docs.oracle.com/javase/7/docs/api/java/util/Locale.html

---
+++ src/main/java/edu/insf/toolkit/Tools/Languages.java
@@ -0,0 +1,15 @@
+package edu.insf.toolkit.Tools;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jamaaltaylor
+ * Date: 1/19/14
+ * Time: 6:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Languages
+{
+    public static String ENGLISH = "eng";
+
+    public static String SPANISH = "spa";
+}
