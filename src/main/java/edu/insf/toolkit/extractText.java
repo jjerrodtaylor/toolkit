9b7fd74d3554c4e4a840dcb1bbe24347dd6bd229
jamaal.taylor <j.jerrod.taylor@gmail.com>
2014-01-21 19:25:32 -0500

    I'm trying to work on processing chapters now instead of pages. I'm running into problems but I'll work on them tomorrow.

---
+++ src/main/java/edu/insf/toolkit/extractText.java
@@ -0,0 +1,45 @@
+package edu.insf.toolkit;
+
+import edu.insf.toolkit.DesignPatterns.FluidInterfaceHTML.HTMLPage;
+import edu.insf.toolkit.Models.BPage;
+import edu.insf.toolkit.Models.Chapter;
+import edu.insf.toolkit.Models.ParallelPage;
+import edu.insf.toolkit.Tools.Constants;
+import edu.insf.toolkit.Tools.FileHelper;
+import edu.insf.toolkit.Tools.Languages;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class extractText {
+
+    public static void main(final String args[])
+    {
+        FileHelper fileHelper = new FileHelper();
+        BPage firstChapter;
+
+        //create the dirctory in raw if it doesn't exist
+        //turn this into function later
+        Constants.makeDirectory(fileHelper.turnToFile(Constants.rawFilePath(Languages.ENGLISH)));
+        Constants.makeDirectory(fileHelper.turnToFile(Constants.rawFilePath(Languages.SPANISH)));
+        //For the english pg 0-8 ch.1
+
+        //get the text from the pdfs
+        for(int i=0;i<8;i++)
+        {
+            firstChapter = new BPage();
+            firstChapter.getPDFTextByPage(fileHelper.turnToFile(Constants.pdfFilePath("metamorph_eng.pdf")),i);
+            fileHelper.writeFile(firstChapter.getUnTokenizedPage(),Constants.rawFilePath("/eng/metamorph_eng_"+String.valueOf(i)+".txt"));
+        }
+
+        //1-9 for the spanish
+        for(int i=1;i<10;i++)
+        {
+            firstChapter = new BPage();
+            firstChapter.getPDFTextByPage(fileHelper.turnToFile(Constants.pdfFilePath("metamorph_esp.pdf")),i);
+            fileHelper.writeFile(firstChapter.getUnTokenizedPage(),Constants.rawFilePath("/spa/metamorph_esp_"+String.valueOf(i)+".txt"));
+        }
+
+        //do manual editing at this step.
+    }
+}
