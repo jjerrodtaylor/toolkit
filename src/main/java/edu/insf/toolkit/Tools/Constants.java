fdce38c59f7dd198697859ab21ceb1e6e0c7aa5a
jamaal.taylor <j.jerrod.taylor@gmail.com>
2014-01-21 19:23:42 -0500

    Added functions to make directories as well as returning paths to folders.

--- src/main/java/edu/insf/toolkit/Tools/Constants.java
+++ src/main/java/edu/insf/toolkit/Tools/Constants.java
@@ -7,16 +7,25 @@ import java.io.File;
  */
 public class Constants
 {
+    private static String filePath = "";
+
     public static String READ_FILE_NAME = null;
     public static String WRITE_FILE_NAME = null;
-    public static String METAMORPH_ENG = Constants.makeFilePath("/src/test/resources/metamorph_eng.pdf");
-    public static String METAMORPH_ESP = Constants.makeFilePath("/src/test/resources/metamorph_esp.pdf");
-    public static String METAMORPH_ENG_TXT = Constants.makeFilePath("/src/test/resources/metamorph_eng.txt");
-    public static String METAMORPH_ESP_TXT = Constants.makeFilePath("/src/test/resources/metamorph_esp.txt");
-    public static String METAMORPH_ENG_TXT_FMT = Constants.makeFilePath("/src/test/resources/metamorph_eng_fmt.txt");
-    public static String METAMORPH_ESP_TXT_FMT = Constants.makeFilePath("/src/test/resources/metamorph_esp_fmt.txt");
+    public static String RAW = Constants.makeFilePath("/src/test/resources/raw/");
+    public static String PDF = Constants.makeFilePath("/src/test/resources/pdf/");
+    public static String PARTIALLY_PROCESSED = Constants.makeFilePath("/src/test/resources/partiallyprocessed/");
+    public static String FULLY_PROCESSED = Constants.makeFilePath("/src/test/resources/fullyprocessed/");
+    public static String ALIGNED = Constants.makeFilePath("/src/test/resources/aligned/");
+    public static String ODT = Constants.makeFilePath("/src/test/resources/odt");
+
+    public static String METAMORPH_ENG = Constants.makeFilePath("/src/test/resources/pdf/metamorph_eng.pdf");
+    public static String METAMORPH_ESP = Constants.makeFilePath("/src/test/resources/pdf/metamorph_esp.pdf");
+    public static String METAMORPH_ENG_TXT = Constants.makeFilePath("/src/test/resources/raw/metamorph_eng.txt");
+    public static String METAMORPH_ESP_TXT = Constants.makeFilePath("/src/test/resources/raw/metamorph_esp.txt");
+    public static String METAMORPH_ENG_TXT_FMT = Constants.makeFilePath("/src/test/resources/partiallyprocessed/metamorph_eng_fmt.txt");
+    public static String METAMORPH_ESP_TXT_FMT = Constants.makeFilePath("/src/test/resources/partiallyprocessed/metamorph_esp_fmt.txt");
     public static String TEST_FILE = Constants.makeFilePath("/src/test/resources/testfile.txt");
-    public static String METAMORPH_ENG_ESP_TXT = Constants.makeFilePath("/src/test/resources/metamorph-en-es.txt");
+    public static String METAMORPH_ENG_ESP_TXT = Constants.makeFilePath("/src/test/resources/aligned/metamorph-en-es.txt");
 
     public static String makeFilePath(String addition)
     {
@@ -25,13 +34,43 @@ public class Constants
         return filePath;
     }
 
-    public void setREAD_FILE_NAME(String file_name)
+    public static String pdfFilePath(String nameOfFile)
     {
-        READ_FILE_NAME = file_name;
+        return Constants.filePath = Constants.PDF+nameOfFile;
     }
 
-    public void setWRITE_FILE_NAME(String file_name)
+    public static String rawFilePath(String nameOfFile)
     {
-        WRITE_FILE_NAME = file_name;
+        return Constants.filePath = Constants.RAW+nameOfFile;
+    }
+
+    public static String partiallyprocessedFilePath(String nameOfFile)
+    {
+        return Constants.filePath = Constants.PARTIALLY_PROCESSED+nameOfFile;
+    }
+
+    public static String fullyprocessedFilePath(String nameOfFile)
+    {
+        return Constants.filePath = Constants.FULLY_PROCESSED+nameOfFile;
+    }
+
+    public static String alignedFilePath(String nameOfFile)
+    {
+        return Constants.filePath = Constants.ALIGNED+nameOfFile;
+    }
+
+    public static String odtFilePath(String nameOfFile)
+    {
+        return Constants.filePath = Constants.ODT+nameOfFile;
+    }
+
+    public static boolean makeDirectory(File dir)
+    {
+        boolean result = false;
+        if(!dir.exists())
+        {
+            result = dir.mkdir();
+        }
+        return result;
     }
 }
